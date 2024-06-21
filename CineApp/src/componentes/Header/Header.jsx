e91bf8feb0d5f9d6cbf872ec065c7416f91db719
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-21 11:13:06 -0300

    quase la

--- CineApp/src/componentes/Header/Header.jsx
+++ CineApp/src/componentes/Header/Header.jsx
@@ -2,9 +2,9 @@ import Navbar from "../Navbar/Navbar";
 
 function Header() {
     return ( 
-        <header className="flex bg-black p-4 text-white justify-around">
+        <header className="flex bg-amber-500 p-4 text-white justify-around 	border-radius: 0.125rem;">
             <span>Olá, visitante!</span>
-            <h1>Bem vindo ao Chinema</h1>
+            <h1>Bem vindo ao Cinema</h1>
             <Navbar></Navbar>
         </header>
      );
--- CineApp/src/componentes/Navbar/Navbar.jsx
+++ CineApp/src/componentes/Navbar/Navbar.jsx
@@ -5,8 +5,7 @@ function Navbar() {
             <ul className='flex gap-3'>
                 <li><Link to ="/">Home</Link></li>
                 <li><Link to ="filmes">Filmes</Link></li>
-                <li><Link to ="sobre">Sobre</Link></li>
-                <li><Link to ="contato">Contato</Link></li>
+
             </ul>
         </nav>
      );
